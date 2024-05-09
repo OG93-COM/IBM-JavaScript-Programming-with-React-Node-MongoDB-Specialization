08e830f6d71d80c95a6c01e72c7e0d39a9efc265
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-09 19:31:48 +0100

    TravelOG

---
+++ JavaScriptEssentials/ProjectFinal/travel_recommendation.js
@@ -0,0 +1,8 @@
+const btnSearch = document.getElementById('btnSearch');
+
+const plan = [];
+
+function searchPlan() {
+    const inputSearch = document.getElementById('destinationInput').value.toLowerCase();
+    const resultSearch = document.getElementById('result');
+}
