90315124a706bd794e32303e6ab46d39faf59122
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-09 19:43:05 +0100

    TravelOG

--- JavaScriptEssentials/ProjectFinal/travel_recommendation.js
+++ JavaScriptEssentials/ProjectFinal/travel_recommendation.js
@@ -4,5 +4,22 @@ const plan = [];
 
 function searchPlan() {
     const inputSearch = document.getElementById('destinationInput').value.toLowerCase();
-    const resultSearch = document.getElementById('result');
-}
+    const resultDiv = document.getElementById('result');
+    resultDiv = '';
+    fetch('./travel_recommendation_api.json')
+          .then(response => response.json())
+          .then(data => {
+            const countries = data.countries.find(item => item.name.toLowerCase() === inputSearch );
+            if (countries) {
+              resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
+            } else {
+              
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+          });
+      }
+
+    btnSearch.addEventListener('click', searchPlan);
