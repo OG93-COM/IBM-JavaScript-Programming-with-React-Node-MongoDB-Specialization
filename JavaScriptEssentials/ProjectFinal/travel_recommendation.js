3195bc9d878a122b180923ff03a03effc236dce1
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-10 01:49:51 +0100

    TravelOG

--- JavaScriptEssentials/ProjectFinal/travel_recommendation.js
+++ JavaScriptEssentials/ProjectFinal/travel_recommendation.js
@@ -1,62 +1,78 @@
 const btnSearch = document.getElementById('btnSearch');
 const btnReset = document.getElementById('btnReset');
 
-function displayCity(city) {
-    return `
-    <div class="leftContent">
-        <img src="${city.imageUrl}" alt="">
-        <div class="info">
-            <h2>${city.name}</h2>
-            <p>${city.description}</p>
-            <button class="btnVisit">Visit</button>
-        </div>
-    </div>`;
-}
-
 function searchPlan() {
     const inputSearch = document.getElementById('destinationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
     fetch('./travel_recommendation_api.json')
-        .then(response => response.json())
-        .then(data => {
-            let urlImage = '';
-            let cityResult = '';
+          .then(response => response.json())
+          .then(data => {
+            
+            const beaches = data.beaches.filter(item => item.name.toLowerCase().includes(inputSearch));
+            const temples = data.temples.filter(item => item.name.toLowerCase().includes(inputSearch));
+            console.log(temples);
 
+            let cityResult = '';
             data.countries.forEach(country => {
                 country.cities.forEach(city => {
                     if (city.name.toLowerCase().includes(inputSearch)) {
-                        urlImage = city.imageUrl;
-                        cityResult += displayCity(city);
+                        const urlImage = city.imageUrl;
+                        cityResult += `
+                            <div class="leftContent">
+                                <img src="${urlImage}" alt="">
+                                <div class="info">
+                                    <h2>${city.name}</h2>
+                                    <p>${city.description}</p>
+                                    <button id="btnVisit">Visit</button>
+                                </div>
+                            </div>`;
                     }
                 });
             });
 
-            const countries = data.countries.find(item => item.name.toLowerCase().includes(inputSearch));
-            const beaches = data.beaches.find(item => item.name.toLowerCase().includes(inputSearch));
-            const temples = data.temples.find(item => item.name.toLowerCase().includes(inputSearch));
-
-            if (countries) {
-                resultDiv.innerHTML += displayCity(countries.cities[0]);
+            if (beaches.length > 0) {
+                beaches.forEach(beach => {
+                    resultDiv.innerHTML += `
+                        <div class="leftContent">
+                            <img src="${beach.imageUrl}" alt="">
+                            <div class="info">
+                                <h2>${beach.name}</h2>
+                                <p>${beach.description}</p>
+                                <button id="btnVisit">Visit</button>
+                            </div>
+                        </div>`;
+                });
+            } else if (temples.length > 0) {
+                temples.forEach(temple => {
+                    resultDiv.innerHTML += `
+                        <div class="leftContent">
+                            <img src="${temple.imageUrl}" alt="">
+                            <div class="info">
+                                <h2>${temple.name}</h2>
+                                <p>${temple.description}</p>
+                                <button id="btnVisit">Visit</button>
+                            </div>
+                        </div>`;
+                });
             } else if (cityResult !== '') {
                 resultDiv.innerHTML += cityResult;
-            } else if (beaches) {
-                resultDiv.innerHTML += displayCity(beaches);
-            } else if (temples) {
-                resultDiv.innerHTML += displayCity(temples);
             } else {
-                resultDiv.innerHTML = '<div class="leftContent info"><h2>Please add a valid search term</h2></div>';
+                resultDiv.innerHTML = '<div class="leftContent info"><h2>Please Add valid search</h2></div>';
             }
-        })
-        .catch(error => {
+          })
+          .catch(error => {
             console.error('Error:', error);
             resultDiv.innerHTML = 'An error occurred while fetching data.';
-        });
+          });
 }
 
 btnSearch.addEventListener('click', searchPlan);
 
-btnReset.addEventListener('click', () => {
-    document.getElementById('result').innerHTML = '';
-});
+function reselAll() {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+}
+
+btnReset.addEventListener('click', reselAll);
