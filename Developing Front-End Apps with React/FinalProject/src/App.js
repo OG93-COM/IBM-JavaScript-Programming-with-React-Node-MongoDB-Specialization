aab09fc788f6c8acc9c0f4cff1aeb8445ed2bcc6
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-07-07 14:05:46 +0100

    FinalProject

--- Developing Front-End Apps with React/FinalProject/src/App.js
+++ Developing Front-End Apps with React/FinalProject/src/App.js
@@ -1,43 +1,44 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        <Budget/>
+                    {/*     // Budget component */}
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    {/* //Remaining component */}
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    {/* //ExpenseTotal component */}
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    {/* //ExpenseList component */}
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                   {/* //ExpenseItems component */}
+                   <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
 
                 </div>
+                {/* //AllocationForm component */}
+                        <AllocationForm />
             </div>
         </AppProvider>
     );
--- Developing Front-End Apps with React/FinalProject/src/components/Budget.js
+++ Developing Front-End Apps with React/FinalProject/src/components/Budget.js
@@ -1,10 +1,16 @@
-
-import React from 'react'
-
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 const Budget = () => {
-  return (
-    <div>Budget</div>
-  )
-}
-
-export default Budget
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
